ef37e2a174415d8e715dc398273eb5a7976fb40e
Sven Thiele <sthiele78@gmail.com>
2015-05-13 14:08:19 +0200

    added new parsers for matrix and properties

--- CSVParser.hs
+++ CSVParser.hs
@@ -16,10 +16,13 @@
 -- along with xxxx.  If not, see <http://www.gnu.org/licenses/>.
 
 module CSVParser (
-    readCSV
+    readCSV,
+    readCSV2,
+    readProps,
 ) where
 import Graph
 import Data.Attoparsec.Text
+import Data.Attoparsec.ByteString.Char8(isSpace)
 import Control.Applicative ((<|>))
 import Data.Text (pack)
 import Data.Scientific as Scientific
@@ -43,7 +46,8 @@ edgeParser = do
   w <- Data.Attoparsec.Text.scientific
   return (Edge n1 n2 sign (toRealFloat w))
   
-  
+
+
 nodeParser:: Parser [Char]
 nodeParser = do
   n <- identifier
@@ -77,13 +81,103 @@ mchar =
   do
     c <- char '_'
     return c
+  <|>
+  do
+    c <- char '-'
+    return c
+  <|>
+  do
+    c <- char '.'
+    return c
 
+    
+identifierr:: Parser String
+identifierr = do
+  l <-letter
+  s <- (many1 mchar)
+  return (l:s)
 
 -----------                   
 readCSV input =  parseOnly csvParser (Data.Text.pack input)
 
 
 
+skipSpaceNoNewline = skipWhile (\x -> isSpace x && not (isEndOfLine x))
+
+type MyMAT = ([String],[String],[[Double]])
+
+csvParser2:: Parser MyMAT
+csvParser2 =
+--   let (rowids,dta) = unscramble rows in
+  do
+  columnids <- headerParser
+  endOfLine
+  rows <- (many' rowParser)
+  let (rowids,dta) = unscramble rows in
+      return (columnids, rowids, dta)
+
+headerParser:: Parser [String]
+headerParser =
+  do
+  skipSpaceNoNewline
+  columnids <- (many' colidParser)
+  return columnids
+  
+type MyROW = (String,[Double])
+
+colidParser:: Parser String
+colidParser = do
+  colid <- identifierr
+  skipSpaceNoNewline 
+  return colid
 
 
+rowParser:: Parser MyROW
+rowParser = do
+  skipSpaceNoNewline
+  rowid <- identifierr
+  skipSpaceNoNewline
+  dta <- (many' cellParser)
+  endOfLine
+  return (rowid, dta)
 
+
+cellParser:: Parser Double
+cellParser = do
+  cell <- Data.Attoparsec.Text.scientific
+  skipSpaceNoNewline
+  return (toRealFloat cell)
+  
+
+unscramble:: [MyROW] -> ([String],[[Double]])
+unscramble [] = ([],[])
+unscramble ((id,dta):rest) =
+  let (ids,dtas) = unscramble rest in
+      ((id:ids),(dta:dtas))
+      
+readCSV2 input = parseOnly csvParser2 (Data.Text.pack input)
+
+readProps input = parseOnly propsParser (Data.Text.pack input)
+
+propsParser:: Parser [(String,String)]
+propsParser =
+  do
+  props <- (many' propParser)
+  return props
+
+propParser:: Parser (String,String)
+propParser = do
+  skipSpaceNoNewline
+  node <- identifierr
+  skipSpaceNoNewline
+  prop <- propertyParser
+  endOfLine
+  return (node, prop)
+
+anyNoNewline = satisfy (not . isEndOfLine)
+
+
+propertyParser:: Parser String
+propertyParser = do
+  property <- (many' anyNoNewline)
+  return property
